de58a5682811cbc83b9f5cd302601e4047dcc571
Martin Miglio <marmig0404@gmail.com>
2022-04-28 03:35:09 -0400

    added unit tests

---
+++ test/testvoxel.py
@@ -0,0 +1,9 @@
+import unittest
+from stlinmc import voxel
+
+class TestMain(unittest.TestCase):
+    def test_voxel():
+        # https://commons.wikimedia.org/wiki/File:Stanford_Bunny.stl
+        voxel.import_stl_as_voxels('data/Stanford_Bunny.stl')
+        # https://reprap.org/forum/read.php?88,6830
+        voxel.import_stl_as_voxels('data/HalfDonut.stl')
