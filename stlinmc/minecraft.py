740ba05c5ee01ee0705e104133a66d44945d4005
Martin Miglio <marmig0404@gmail.com>
2022-04-28 00:04:22 -0400

    added function to send voxels to raspberry juice

---
+++ stlinmc/minecraft.py
@@ -0,0 +1,18 @@
+from mcpi import block, connection, minecraft
+from .voxel import import_stl_as_voxels
+
+
+def build_voxels(voxels, server_ip, server_port=4711):
+    conn = connection.Connection(server_ip, server_port)
+    mc = minecraft.Minecraft(conn)
+    x, y, z = mc.player.getTilePos()
+    for layer_index, layer in enumerate(voxels):
+        for row_index, row in enumerate(layer):
+            for column_index, column in enumerate(row):
+                xc = x+column_index
+                yc = y + layer_index
+                zc = z + row_index
+                if column == 1:
+                    mc.setBlock(xc, yc, zc, block.COBBLESTONE.id)
+                else:
+                    mc.setBlock(xc, yc, zc, block.AIR.id)
