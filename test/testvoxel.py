cc4a64cbbc3cbd45c7290a22c9963d52856d219d
Martin Miglio <marmig0404@gmail.com>
2022-04-28 03:46:55 -0400

    fixed unit test

--- test/testvoxel.py
+++ test/testvoxel.py
@@ -3,7 +3,7 @@ from stlinmc import voxel
 
 
 class TestMain(unittest.TestCase):
-    def test_voxel():
+    def test_voxel(self):
         # https://commons.wikimedia.org/wiki/File:Stanford_Bunny.stl
         voxel.import_stl_as_voxels('data/Stanford_Bunny.stl')
         # https://reprap.org/forum/read.php?88,6830
