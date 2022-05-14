8bac368512a1d676a389b76a06af2f28640b7907
Martin Miglio <marmig0404@gmail.com>
2022-05-14 01:43:50 -0400

    add importing of files from text bumped version

--- stlinmc/voxel.py
+++ stlinmc/voxel.py
@@ -10,7 +10,7 @@ from numpy import hstack, newaxis
 
 def import_stl_as_voxels(input_file_path, parallel):
     """imports an stl file into a voxel array
-F
+
     Args:
         input_file_path (str): path to stl file
 
@@ -20,6 +20,27 @@ F
     """
     meshes = []
     mesh_obj = Mesh.from_file(input_file_path)
+    org_mesh = hstack((
+        mesh_obj.v0[:, newaxis],
+        mesh_obj.v1[:, newaxis],
+        mesh_obj.v2[:, newaxis]
+    ))
+    meshes.append(org_mesh)
+    vol = convert_meshes(meshes, resolution=100, parallel=parallel)[0]
+    return vol
+
+def import_text_as_voxels(input_text, parallel):
+    """imports an stl file into a voxel array
+
+    Args:
+        input_text (str): stl text
+
+    Returns:
+        nDArray[int8]: a set of voxels in a 3D array,
+                          0 and 1 indicate air and block respectively
+    """
+    meshes = []
+    mesh_obj = Mesh.from_file(None, fh=input_text)
     org_mesh = hstack((
         mesh_obj.v0[:, newaxis],
         mesh_obj.v1[:, newaxis],
